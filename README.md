3e77376fa9f57bb522b8372b8db851abb37f3253
Jeff Andrews <jeff@TFS2010Builds-iMac.local>
2016-10-12 15:28:11 +1300

    Expanded information reported by tool. Accepted multiple package names. Made failure more robust (reports missing packages)

--- README.md
+++ README.md
@@ -2,12 +2,26 @@
 Generate a simple report containing information about all the dependencies of an npm package.
 
 ## Usage
-Currently: `node index.js [package name]`
+Currently: `node index.js [package names...]`
 
 ```
-$ node index.js jquery
-SUCCESS
-Package 'jquery' Last published 2016-09-22T22:32:49.360Z by 'timmywil'
+$ node index.js jquery nopt
+==================================================
+(1/2) 200 SUCCESS 
+PACKAGE: jquery
+Last published 2016-09-22T22:32:49.360Z by 'timmywil'
+Latest Version: 3.1.1
+GitHub: github.com/jquery/jquery
+License: MIT
+
+==================================================
+(2/2) 200 SUCCESS 
+PACKAGE: nopt
+Last published 2015-11-12T21:58:26.454Z by 'othiym23'
+Latest Version: 3.0.6
+GitHub: github.com/npm/nopt
+License: ISC
+
 ```
 
 Just outputs last published date.
--- index.js
+++ index.js
@@ -2,24 +2,51 @@ var request = require('request');
 var cheerio = require('cheerio');
 var argv = require('yargs').argv;
 
-var packageName = argv._[0];
+var packageNames = argv._;
 
-request('https://www.npmjs.com/package/' + packageName, function (error, response, body) {
-    if (error) {
-        throw new Error(error);
-    } else {
-        if (!response.statusCode === 200) {
-            throw new Error("Bad status code: " + response.statusCode);
+var completedModules = 0;
+
+
+packageNames.forEach(function(packageName) {
+    request('https://www.npmjs.com/package/' + packageName, function (error, response, body) {
+        var progressString = "(" + (++completedModules) + "/" + packageNames.length + ")";
+
+
+        console.log("==================================================");
+
+        if (error) {
+            throw new Error(error);
         } else {
-            console.log("SUCCESS");
+            if (response.statusCode === 404) {
+                console.error(progressString + " " + response.statusCode + " FAILURE: Package '" + packageName + "' does not exist");
+            } else if (response.statusCode !== 200) {
+                console.error(response.statusCode + " FAILURE: Bad status code");
+            } else {
+                console.log(progressString + " " + response.statusCode + " SUCCESS ");
 
-            var $ = cheerio.load(body);
+                var $ = cheerio.load(body);
 
-            var $lastPublisher = $('li.last-publisher');
-            var publishedBy = $lastPublisher.find('a span').text().trim();
-            var publishedTime = $lastPublisher.children("span").text().trim();
+                var $lastPublisher = $('li.last-publisher');
+                var publishedBy = $lastPublisher.find('a span').text().trim();
+                var publishedTime = $lastPublisher.children("span").text().trim();
+                
+                var $releases = $lastPublisher.next();
+                var latestRelease = $releases.find('strong').text().trim();
 
-            console.log("Package '" + packageName + "' Last published " + publishedTime + " by '" + publishedBy + "'");
+                var $github = $releases.next();
+                var githubUrl = $github.find('a').text().trim()
+
+                var $license = $github.next();
+                var licenseName = $license.find('a').text().trim();
+
+                console.log("PACKAGE: " + packageName);
+                console.log("Last published " + publishedTime + " by '" + publishedBy + "'");
+                console.log("Latest Version: " + latestRelease);
+                console.log("GitHub: " + githubUrl);
+                console.log("License: " + licenseName);
+            }
         }
-    }
+
+        console.log("");
+    });
 });
