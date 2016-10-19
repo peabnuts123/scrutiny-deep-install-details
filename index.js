3e6ad26a2d1098831b2e7539a10176f9d2d8ee43
Jeff Andrews <peabnuts123@gmail.com>
2016-10-19 15:54:59 +1300

    Changed source of data to npm registry API. Removed dodgey web requests. Added some new fields

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var argv = require('yargs').argv;
+var _ = require('underscore');
+
+var Registry = require('npm-registry');
+
+var npm = new Registry({
+    registry: 'https://registry.npmjs.org'
+});
+
 
 var packageNames = argv._;
 
@@ -8,45 +16,88 @@ var completedModules = 0;
 
 
 packageNames.forEach(function(packageName) {
-    request('https://www.npmjs.com/package/' + packageName, function (error, response, body) {
-        var progressString = "(" + (++completedModules) + "/" + packageNames.length + ")";
+    //Kick off a bunch of requests to the npm registry API 
 
-
-        console.log("==================================================");
-
-        if (error) {
-            throw new Error(error);
-        } else {
-            if (response.statusCode === 404) {
-                console.error(progressString + " " + response.statusCode + " FAILURE: Package '" + packageName + "' does not exist");
-            } else if (response.statusCode !== 200) {
-                console.error(response.statusCode + " FAILURE: Bad status code");
+    //Get Dependents of a Package
+    var dependentsPromise = new Promise(function (resolve, reject) {
+        npm.packages.depended(packageName, function (err, dependents) {
+            if (err) {
+                reject(err);
             } else {
-                console.log(progressString + " " + response.statusCode + " SUCCESS ");
-
-                var $ = cheerio.load(body);
-
-                var $lastPublisher = $('li.last-publisher');
-                var publishedBy = $lastPublisher.find('a span').text().trim();
-                var publishedTime = $lastPublisher.children("span").text().trim();
-                
-                var $releases = $lastPublisher.next();
-                var latestRelease = $releases.find('strong').text().trim();
-
-                var $github = $releases.next();
-                var githubUrl = $github.find('a').text().trim()
-
-                var $license = $github.next();
-                var licenseName = $license.find('a').text().trim();
-
-                console.log("PACKAGE: " + packageName);
-                console.log("Last published " + publishedTime + " by '" + publishedBy + "'");
-                console.log("Latest Version: " + latestRelease);
-                console.log("GitHub: " + githubUrl);
-                console.log("License: " + licenseName);
+                resolve(dependents);
             }
-        }
+        });
+    });
 
-        console.log("");
+    //Get Details of Package (name, author, dependencies etc)
+    var newDetailsPromise = new Promise(function (resolve, reject) {
+        npm.packages.details(packageName, function (error, data) {
+            if (error) {
+                reject(error);
+            } else {
+                var details = data[0];
+
+                //Pluck / parse the details we want from the JSON return by the API
+                resolve({
+                    name: details.latest.name,
+                    publishDate: details.time[details.latest.version] ? new Date(details.time[details.latest.version]) : undefined, 
+                    publishAuthor: details.releases[details.latest.version].name,
+                    latestVersion: details.latest.version,
+                    repositoryURL: details.latest.repository.url,
+                    homepage: details.latest.homepage,
+                    license: details.license,
+                    dependencies: _.reduce(details.latest.dependencies, function(current, value, key){
+                        current.push({version: value, name: key});
+                        return current;
+                    }, []),
+                    devDependencies: _.reduce(details.latest.devDependencies, function(current, value, key){
+                        current.push({version: value, name: key});
+                        return current;
+                    }, [])
+                });
+            }
+        });
+    });
+
+    Promise.all([dependentsPromise, newDetailsPromise]).then(function onResolve(resolveObjects) {
+        var dependents = resolveObjects[0];
+        var detailsObject = resolveObjects[1];
+
+        detailsObject.dependents = dependents;
+
+        summarisePackage(undefined, detailsObject);
+    }, function onReject(error) {
+        summarisePackage(error);
+    }).catch(function(error) {
+        console.error(error.stack);
     });
 });
+
+function summarisePackage(error, pkgInfo) {
+    var progressString = "(" + (++completedModules) + "/" + packageNames.length + ")";
+
+    console.log("========================= " + pkgInfo.name + " =========================");
+    if (error) {
+        console.log(progressString + " FAILURE");
+        console.error(error);
+        return;
+    } 
+
+    console.log(progressString + " SUCCESS ");
+    with (pkgInfo) {
+        console.log(`
+Last Published: ${publishDate} by ${publishAuthor}
+Latest Version: ${latestVersion}
+Repository URL: ${repositoryURL}
+Homepage: ${homepage}
+License: ${license}
+# Dependencies (Direct): ${dependencies.length}
+# DevDependencies (Direct): ${devDependencies.length}
+# Dependents (Direct): ${dependents.length}
+`.trim() + '\n');
+    }
+}
+
+function DEBUG(s) {
+    console.log("DEBUG: " + s);
+}
